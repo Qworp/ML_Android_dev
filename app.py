d0d563337fddc87b33d0d65e7fa29b17cbdd9384
Seun Lawal <35162819+seunss@users.noreply.github.com>
2020-01-09 16:54:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request, jsonify
 
+app = Flask(__name__)
+# Post Request
+# process audiofile
+# return 1 for unhealthy and Return 
 
 @app.route('/')
 def hello_world():
     return 'Hello, Flask! for Heart Classification Project Backend'
+
 
 if __name__ == '__main__':
     app.run()
