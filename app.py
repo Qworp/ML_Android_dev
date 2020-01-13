b5ccd310509ad2083f50b4b4c1c59120b531e8ca
Seun Lawal <35162819+seunss@users.noreply.github.com>
2020-01-13 18:07:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 # Post Request
 # process audiofile
 # return 1 for unhealthy and Return 
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def hello_world():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            #flash('No file part')
+            return redirect(request.url)
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return "Healthy"
     return 'Hello, Flask! for Heart Classification Project Backend'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
