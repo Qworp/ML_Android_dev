53803a61582b4262603d936a0ca7f43f8173d5e3
Seun Lawal <35162819+seunss@users.noreply.github.com>
2020-01-20 19:21:31 -0500

    Machine Learning Model attempt
    Trying to host the model on the flask server

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask
 from flask import request, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename
+#from keras.models import load_model
+import pickle
+
+
 
 app = Flask(__name__)
 # Post Request
 # process audiofile
 # return 1 for unhealthy and Return 
+#model = load_model('model.h5')
+filename = "heartbeat_disease_model_pkl.sav"
+loaded_model = pickle.load(open(filename, 'rb'))
 
-@app.route('/', methods = ['POST'])
+@app.route('/server', methods = ['POST'])
 def hello_world():
     if request.method == 'POST':
         if 'file' not in request.files:
             #flash('No file part')
             return redirect(request.url)
         f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return "Healthy"
+
+        
+        return "Healthy" + f
     return 'Hello, Flask! for Heart Classification Project Backend'
 
 
