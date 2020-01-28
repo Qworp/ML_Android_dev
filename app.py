9cb3dec4d7c00981547adafcd48135dc51fab112
Seun Lawal <35162819+seunss@users.noreply.github.com>
2020-01-27 21:09:01 -0500

    cedric

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename
 #from keras.models import load_model
 import pickle
+from flask import render_template
 
 
 
@@ -11,10 +12,10 @@ app = Flask(__name__)
 # process audiofile
 # return 1 for unhealthy and Return 
 #model = load_model('model.h5')
-filename = "heartbeat_disease_model_pkl.sav"
-loaded_model = pickle.load(open(filename, 'rb'))
+#filename = "heartbeat_disease_model_pkl.sav"
+#loaded_model = pickle.load(open(filename, 'rb'))
 
-def pAudio():
+#def pAudio():
     
 
 @app.route('/server', methods = ['POST'])
@@ -22,16 +23,13 @@ def hello_world_sever():
     if request.method == 'POST':
         if 'file' not in request.files:
             #flash('No file part')
-            return redirect(request.url)
-        f = request.files['file']
 
-        
-        return "Healthy" + f
-    return 'Hello, Flask! for Heart Classification Project Backend'
+        #f = request.files['file']
+         return 'You have a healthy Heart'
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask! for Heart Classification Project Backend'
+    return render_template("kobe.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
